62c2ea0b8c86549d0aea75a8ae33dc9f42a708c6
Zaituny <mohamedinv.580@gmail.com>
2023-04-19 13:47:43 +0200

    Update recording.py

--- Code_tests/recording.py
+++ Code_tests/recording.py
@@ -18,13 +18,13 @@ def count_pixels(img, angle, start_point, color_range,step_size):
     distance = 0
     current_point = (0, 0)
     for i in range(0, 321):
-        # if (current_point[0] >= 0 and current_point[1] >= 0) and all(img[current_point[1], current_point[0]] >= color_range[0]) and all(img[current_point[1], current_point[0]] <= color_range[1]):
-        #     cv2.line(img, (320, 478), current_point, (255, 0, 0), 2)
-        #     return distance
+        if (current_point[0] >= 0 and current_point[1] >= 0) and all(img[current_point[1], current_point[0]] >= color_range[0]) and all(img[current_point[1], current_point[0]] <= color_range[1]):
+            cv2.line(img, (320, 478), current_point, (255, 0, 0), 2)
+            return distance
         current_point = np.array([np.abs(start_point[0] + i * step_size),
                                 int(start_point[1] - grad * (i))])
         distance += 1
-    cv2.line(img, (320, 478), current_point, (0, 255, 0), 2)
+
     return None
     
 def screen_record():
@@ -43,7 +43,7 @@ def screen_record():
         print(count_pixels(printscreen,  45, (320, 478), ((10, 15, 30), (50, 55, 70)),  1))
         print(count_pixels(printscreen,  30, (320, 478), ((10, 15, 30), (50, 55, 70)),  1))
         print(count_pixels(printscreen,  15, (320, 478), ((10, 15, 30), (50, 55, 70)),  1))        
-        #cv2.line(printscreen, (320, 478), (0, 392), (0, 255, 0), 3)
+        
         cv2.imshow('Trackmania Self Driving car', printscreen)
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
